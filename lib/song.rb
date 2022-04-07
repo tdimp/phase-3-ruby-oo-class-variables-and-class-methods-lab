6a534faa7ee9721e8545c41a875a542a8e5755b1
Thomas Dimperio <twdimperio@gmail.com>
2022-04-07 13:08:19 -0600

    commit tree c4d38348ed6364440f7704d2a1152487f4d1d96c

---
+++ lib/song.rb
@@ -0,0 +1,60 @@
+require "pry"
+
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+
+        @name = name
+        @artist = artist
+        @genre = genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.artist_count
+        artist_count = {}
+        @@artists.each do |artist|
+            if artist_count[artist]
+                artist_count[artist] += 1
+            else
+                artist_count[artist] = 1
+            end
+        end
+        artist_count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+            if genre_count[genre]
+                genre_count[genre] += 1
+            else
+                genre_count[genre] = 1
+            end
+        end
+        genre_count
+    end
+end
+
+song1 = Song.new("Paranoid Android", "Radiohead", "Rock")
+song2 = Song.new("Airbag", "Radiohead", "Rock")
+song3 = Song.new("Dominate", "Morbid Angel", "Metal")
+binding.pry
